5e7ec78e3e7ead0d06349d321983734f29376fbc
Krizzly001 <kgonzalez76099@uat.edu>
2025-03-02 00:05:07 -0800

    update controller

---
+++ Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public abstract class Controller : MonoBehaviour
+{
+    // Variable to hold our Pawn
+    public Pawn pawn;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {      
+    }
+    // Update is called once per frame
+    public virtual void Update()
+    {
+    }
+    // Our child classes MUST override the way they process inputs
+    public abstract void ProcessInputs();
+}
