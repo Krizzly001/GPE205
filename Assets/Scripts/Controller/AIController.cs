1ea76cddafe720778541b62c4559d36a5b09f058
Krizzly001 <kgonzalez76099@uat.edu>
2025-03-02 19:51:05 -0800

    update

--- Assets/Scripts/Controller/AIController.cs
+++ Assets/Scripts/Controller/AIController.cs
@@ -6,12 +6,18 @@ public class AIController : Controller
     public enum AIState{Guard, Chase};
     public AIState currentState;
 
+    public GameObject target;
+
+    private float lastStateChangeTime;
+
+    public float targetDistance;
+
 
     //BLUEPRINTS
     
     public override void Start()
     {
-        currentState = AIState.Guard;
+        ChangeState(AIState.Guard);
     }
 
     public override void Update()
@@ -29,12 +35,20 @@ public class AIController : Controller
             // Do work for Guard
                 DoGuardState();
             // Chekc for transitions
+                if (IsDistanceLessThan(target, targetDistance))
+                {
+                    ChangeState(AIState.Chase);
+                }
             break;
 
             case AIState.Chase:
             // Dow work for chase
                 DoChaseState();
             //check for trasition
+                if (IsDistanceLessThan(target, targetDistance))
+                {
+                    ChangeState(AIState.Guard);
+                }
             break;
                 
         }
@@ -44,9 +58,30 @@ public class AIController : Controller
     public void DoGuardState()
     {
         //Do Nothing
+        Debug.Log("Doing Guard state");
     }
     public void DoChaseState()
     {
         //Do Chase
+        Debug.Log("Doing Chase state");
+    }
+
+
+    public bool IsDistanceLessThan(GameObject target, float distance)
+    {
+        if(Vector3.Distance (pawn.transform.position, target.transform.position) < distance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public virtual void ChangeState (AIState newState)
+    {
+        currentState = newState;
+        lastStateChangeTime = Time.time;
     }
 }
